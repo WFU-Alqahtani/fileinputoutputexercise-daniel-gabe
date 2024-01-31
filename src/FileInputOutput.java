80040722e65c1d31ca74ce27a5f402267d2eefe0
dkm <dohk23@wfu.edu>
2024-01-31 16:44:44 -0500

    finished the requirements part 2

--- src/FileInputOutput.java
+++ src/FileInputOutput.java
@@ -1,42 +1,43 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FileInputOutput {
 
     public static void main(String[] args) {
 //
-        FileInputStream file = null;            //this inputStream is for printing all  line
-        FileInputStream Jimmy = null;           //this inputStream is for printing the movie names
+        FileInputStream myFile = null;
+        FileOutputStream changeFile = null;
 
-        try{
-            file = new FileInputStream("src/moviesDataset.csv");
+        try {
+            myFile = new FileInputStream("moviesDataset.csv");
+            changeFile = new FileOutputStream("moviesDataset.csv",true);
         }
-        catch(java.io.FileNotFoundException x){
-            System.out.println("Sorry, no matching file found. Please restart the program");
-            System.exit(1);
-        }
-        Scanner readFile = new Scanner(file);
-        while(readFile.hasNextLine()){
-            String data = readFile.nextLine();
-            System.out.println(data);
-        }
-
-        try{
-            Jimmy = new FileInputStream("src/moviesDataset.csv");
-        }
-        catch(java.io.FileNotFoundException x){
-            System.out.println("Sorry, no matching file found. Please restart the program");
+        catch (FileNotFoundException nfe){
+            System.out.println("File not found");
             System.exit(1);
         }
 
-        Scanner readFile1 = new Scanner(Jimmy);
-        while(readFile1.hasNextLine()){
-            String data1 = readFile1.nextLine();
-            String[] Array = data1.split(",");
-            System.out.println(Array[2]);
-        }
+        Scanner fileReader = new Scanner(myFile);
+        PrintWriter change = new PrintWriter(changeFile);
 
-        readFile.close();
+        while(fileReader.hasNextLine()){
+            String data = fileReader.nextLine();
+            String[] fail = data.split(",");
+
+            try{
+                int checkNum = Integer.parseInt(fail[4]);
+                change.println(fail[4]);
+            }
+            catch(NumberFormatException nfe){
+                System.out.println("NA");
+            }
+
+        }
+    fileReader.close();
+
     }
 
 }
